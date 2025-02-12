96a0fe94fad5297e386bdc6ddf2ad4139f642f17
BBenc3 <147054662+BBenc3@users.noreply.github.com>
2025-02-12 16:09:02 +0100

    Add background image to body and update theme colors

--- src/Theme/colors.js
+++ src/Theme/colors.js
@@ -3,8 +3,8 @@
 const colors = {
     primary: "#166B6B", // main color used for buttons and text
     secondary: "#FFFFFF", // secondary background color, e.g., for hover states
-    background: "transparent", // general background color for components like AppBar
-    text: "#333333", // general text color
+    background: "#F5F5F5", // general background color for components like AppBar
+    text: "#004C4C", // general text color
     accent: "#FF5722", // an accent color (if needed)
   };
   
